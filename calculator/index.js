435e65a4ad98dd288cb7e3ad3b5616b18ba0b8a1
Vladimir-Kod <kodvv@yandex.ru>
2022-12-13 16:05:07 +0500

    code optimization using functions

--- calculator/index.js
+++ calculator/index.js
@@ -3,38 +3,39 @@ var buttonMinus = document.getElementById("buttonMinus");
 var buttonMultiplay = document.getElementById("buttonMultiplay");
 var buttonDevide = document.getElementById("buttonDevide");
 
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
 
-function onButtonPlus(){
-    var input1 = document.getElementById("input1");
-    var input2 = document.getElementById("input2");
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var sum = number1 + number2;
-    alert(sum);
+function number1(){
+    return Number(input1.value);//функция Number() позволяет из строки чисел сделать числа
+}
+function number2(){
+    return Number(input2.value);
+}
+
+function makeOperation(operationCode){
+    if (operationCode === "+"){
+        alert(number1() + number2());
+    } else if (operationCode === "-"){
+        alert(number1() - number2());
+    } else if (operationCode === "*"){
+        alert(number1() * number2());
+    } else if (operationCode === "/"){
+        alert(number1() / number2());
+    }
+}
+
+function onButtonPlus(){ 
+    makeOperation("+")
 };
 function onButtonMinus(){
-    var input1 = document.getElementById("input1");
-    var input2 = document.getElementById("input2");
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var sum = number1 - number2;
-    alert(sum);
+    makeOperation("-")
 };
 function onButtonMultiplay(){
-    var input1 = document.getElementById("input1");
-    var input2 = document.getElementById("input2");
-    var number1 = Number(input1.value);//функция Number() позволяет из строки чисел сделать числа
-    var number2 = Number(input2.value);
-    var sum = number1 * number2;
-    alert(sum);
+    makeOperation("*")
 };
 function onButtonDevide(){
-    var input1 = document.getElementById("input1");
-    var input2 = document.getElementById("input2");
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var sum = number1 / number2;
-    alert(sum);
+    makeOperation("/")
 };
 
 buttonPlus.addEventListener("click", onButtonPlus);
