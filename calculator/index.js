0fd95451269358ab97834ba6da9c12d6180f6f76
Vladimir-Kod <kodvv@yandex.ru>
2022-12-13 16:35:51 +0500

    optimization using eventObject

--- calculator/index.js
+++ calculator/index.js
@@ -6,39 +6,31 @@ var buttonDevide = document.getElementById("buttonDevide");
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
 
-function number1(){
-    return Number(input1.value);//функция Number() позволяет из строки чисел сделать числа
-}
-function number2(){
-    return Number(input2.value);
-}
 
 function makeOperation(operationCode){
+    var number1 = Number(input1.value);//функция Number() позволяет из строки чисел сделать числа
+    var number2 = Number(input2.value);
+
     if (operationCode === "+"){
-        alert(number1() + number2());
+        alert(number1 + number2);
     } else if (operationCode === "-"){
-        alert(number1() - number2());
+        alert(number1 - number2);
     } else if (operationCode === "*"){
-        alert(number1() * number2());
+        alert(number1 * number2);
     } else if (operationCode === "/"){
-        alert(number1() / number2());
+        alert(number1 / number2);
     }
 }
 
-function onButtonPlus(){ 
-    makeOperation("+")
-};
-function onButtonMinus(){
-    makeOperation("-")
-};
-function onButtonMultiplay(){
-    makeOperation("*")
-};
-function onButtonDevide(){
-    makeOperation("/")
-};
 
-buttonPlus.addEventListener("click", onButtonPlus);
-buttonMinus.addEventListener("click", onButtonMinus);
-buttonMultiplay.addEventListener("click", onButtonMultiplay);
-buttonDevide.addEventListener("click", onButtonDevide);
+
+function onOperationButtonClick(eventObject){
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
+
+buttonPlus.addEventListener("click", onOperationButtonClick);
+buttonMinus.addEventListener("click", onOperationButtonClick);
+buttonMultiplay.addEventListener("click", onOperationButtonClick);
+buttonDevide.addEventListener("click", onOperationButtonClick);
